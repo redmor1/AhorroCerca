422e2653699ee891e54022d07b41cc0a2b29ac1c
redmor1 <lightz17@hotmail.com>
2024-08-24 20:59:42 -0300

    First commit

---
+++ backend/src/main/java/com/redmor/AhorroCerca/AhorroCercaApplication.java
@@ -0,0 +1,13 @@
+package com.redmor.AhorroCerca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhorroCercaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhorroCercaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/redmor/AhorroCerca/AhorroCercaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redmor.AhorroCerca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhorroCercaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
